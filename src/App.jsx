541a5c7146bcc1feafb37f956aa7945bf7a1cc43
Akhil Chauhan <akthehacker11@gmail.com>
2023-09-05 20:53:41 +0530

    seach-box

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <Gallery />
+        {/* <Gallery /> */}
         {/* <SearchBar /> */}
       </div>
     </>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,39 @@
 import React, { useEffect, useState } from "react";
 
 const SearchBar = () => {
-  // const [query, setQuery] = useState("");
-  // const [pics, setPics] = useState([]);
-  // const searchPhotos = async (e) => {
-  //   e.preventDefault();
-  //   const response = await fetch(
-  //     `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=xvGMGxAFGKn5PG7DIOwAWXWkdZZZuOdRjDpOK7KP3cg`
-  //   );
-  //   const json = await response.json();
-  //   if (response.ok) {
-  //     setPics(json.results);
-  //   }
-  //   if (!response.ok) {
-  //     console.log(json.errors);
-  //   }
-  // };
+  const [query, setQuery] = useState("");
+  const [pics, setPics] = useState([]);
+  const searchPhotos = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=xvGMGxAFGKn5PG7DIOwAWXWkdZZZuOdRjDpOK7KP3cg`
+    );
+    const json = await response.json();
+    if (response.ok) {
+      setPics(json.results);
+    }
+    if (!response.ok) {
+      console.log(json.errors);
+    }
+  };
 
   return (
     <>
       <form className="form" onSubmit={searchPhotos}>
-        <label className="label" htmlFor="query">
-          {" "}
-          📷
-        </label>
         <input
           type="text"
           name="query"
-          className="input"
-          placeholder={`Try "dog" or "cat"`}
+          className="search-box"
+          placeholder="search image"
           value={query}
+          autoComplete="off"
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button type="submit" className="button">
+        <button className="search-btn" type="submit">
           Search
         </button>
       </form>
-      {/* <div className="card-list">
+      <div className="card-list">
         {pics.map((pic) => (
           <div className="card" key={pic.id}>
             <img
@@ -48,7 +45,7 @@ const SearchBar = () => {
             ></img>
           </div>
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
